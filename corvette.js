18e8b189b17d80926d2773051fa1106d12131eb4
casmar76 <153526511+casmar76@users.noreply.github.com>
2024-01-07 19:07:29 +0100

    Update corvette.js
    bug fixed Click ID

--- corvette.js
+++ corvette.js
@@ -1,45 +1,44 @@
-// corvette = awesomex10
 document.addEventListener('DOMContentLoaded', function () {
     (function () {
-        var a1b2c3 = new URLSearchParams(window.location.search);
-        
-        function d4e5f6(g7h8i9) {
-            return g7h8i9.replace(/ /g, '_s_').replace(/-/g, '_d_');
+        var corvette = new URLSearchParams(window.location.search);
+
+        function power(ping) {
+            return ping.replace(/ /g, '_s_').replace(/-/g, '_d_');
         }
 
-        if (a1b2c3.has('tid')) {
-            var j0k1l2 = a1b2c3.get('tid');
-            var m3n4o5 = d4e5f6(j0k1l2);
-            a1b2c3.set('tid', m3n4o5);
+        if (corvette.has('tid')) {
+            var pong = corvette.get('tid');
+            var street = power(pong);
+            corvette.set('tid', street);
         }
 
-        var p6q7r8 = a1b2c3.get('gclid') || a1b2c3.get('msclkid') || a1b2c3.get('fbclid');
+        var mango = corvette.get('gclid') || corvette.get('msclkid') || corvette.get('fbclid');
 
-        if (a1b2c3.toString()) {
-            var s9t0u1 = document.getElementsByTagName('a');
-            for (var v2w3x4 = 0; v2w3x4 < s9t0u1.length; v2w3x4++) {
-                var y5z6a7 = s9t0u1[v2w3x4];
-                var b8c9d0 = y5z6a7.hash;
-                var e1f2g3 = y5z6a7.href.split('#')[0];
-                var h4i5j6 = new URL(e1f2g3).searchParams;
+        if (corvette.toString()) {
+            var ciao01 = document.getElementsByTagName('a');
+            for (var bello02 = 0; bello02 < ciao01.length; bello02++) {
+                var winter = ciao01[bello02];
+                var summer = winter.hash;
+                var rocket01 = winter.href.split('#')[0];
+                var mellon = new URL(rocket01, document.location.href).searchParams;
 
-                var k7l8m9 = p6q7r8;
+                var cherry0 = mango;
 
-                if (h4i5j6.has('tid') && p6q7r8) {
-                    k7l8m9 = d4e5f6(p6q7r8);
+                if (mellon.has('tid') && mango) {
+                    cherry0 = power(mango);
                 }
 
-                if (k7l8m9) {
-                    e1f2g3 = e1f2g3.replace('[cnlid]', k7l8m9).replace('%5Bcnlid%5D', k7l8m9);
+                if (cherry0) {
+                    rocket01 = rocket01.replace('[sclid]', cherry0).replace('%5Bssclid%5D', cherry0);
                 }
 
-                var n0o1p2 = a1b2c3.toString();
-                if (e1f2g3.indexOf('?') === -1) {
-                    e1f2g3 += '?' + n0o1p2;
+                var nXzero = corvette.toString();
+                if (rocket01.indexOf('?') === -1) {
+                    rocket01 += '?' + nXzero;
                 } else {
-                    e1f2g3 += '&' + n0o1p2;
+                    rocket01 += '&' + nXzero;
                 }
-                y5z6a7.href = e1f2g3 + b8c9d0;
+                winter.href = rocket01 + summer;
             }
         }
     })();
